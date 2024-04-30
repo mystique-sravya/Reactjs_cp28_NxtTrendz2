fdd01dc29bec4e968d8fbba26090abe46be50b67
mystique-sravya <sravyanalanagula2003@gmail.com>
2024-04-30 12:48:55 +0000

    first commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Cart = () => (
+  <div className="cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="cart-img"
+    />
+  </div>
+)
+
+export default Cart
